48ff0313af249ae32273e72b68bf7f5a17302d2c
LarioDiniz <lario.diniz@palo.com.br>
2018-09-17 15:19:31 -0300

    Seção 01 - Aula 09

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -5,4 +5,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello Word')
+    texts = ['Lorem ipsum dolor sit amet', 
+             'consectetur adipisicing elit',
+             'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.']
+    context = {
+        'title': 'django e-commerce',
+        'texts': texts
+    }
+    return render(request, 'index.html', context)
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+def product(request):
+    return render(request, 'product.html')
