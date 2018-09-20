c59b6dc0a46029d805189bc2f87a54e41d875b05
LarioDiniz <lario.diniz@palo.com.br>
2018-09-20 13:31:35 -0300

    adicionado a biblioteca django-heroku

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,3 +152,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+django_heroku.settings(locals())
